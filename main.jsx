36d7fccabf262ccf33ca46ec084e5a00a7644b2b
Kathleenmac60 <65614148+Kathleenmac60@users.noreply.github.com>
2025-12-13 09:40:26 -0600

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MakupsonFarmWebsite from "./MakupsonFarmWebsite";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MakupsonFarmWebsite />
+  </React.StrictMode>
+);
