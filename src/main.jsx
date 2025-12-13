5c2f5e33bee8971cbf53488639c757b28755a5c9
Kathleenmac60 <65614148+Kathleenmac60@users.noreply.github.com>
2025-12-13 11:28:21 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import MakupsonFarmWebsite from "./MakupsonFarmWebsite";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -8,3 +9,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <MakupsonFarmWebsite />
   </React.StrictMode>
 );
+
